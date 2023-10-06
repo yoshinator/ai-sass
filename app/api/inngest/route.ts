52f8b9aae5a487091bde6d7a5831662b51d196c0
Yoan <yoanante@gmail.com>
2023-10-06 09:00:32 -0400

    Update inngest to version 3
    Update inngets to version 3

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -4,7 +4,7 @@ import { inngest } from '@/inngest/client'
 import { getEmailAddressesFromGmail } from '@/inngest/functions'
 
 // Create an API that serves zero functions
-export const { GET, POST, PUT } = serve(inngest, [
-  /* your functions will be passed here later! */
-  getEmailAddressesFromGmail,
-])
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [getEmailAddressesFromGmail],
+})
